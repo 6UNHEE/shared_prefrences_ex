6e7356e1217b0d0225ae4d29bd9f3a06b71554a5
eunhee <ehy1025@dicamo.com>
2024-05-29 21:56:15 +0900

    drawer 수정

--- lib/sb_house.dart
+++ lib/sb_house.dart
@@ -10,10 +10,35 @@ class SbHouse extends StatefulWidget {
 class _SbHouseState extends State<SbHouse> {
   bool _isChecked = false;
   final _textController = TextEditingController();
+  bool _isChanged = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.language),
+              title: const Text('언어'),
+              trailing: const Icon(Icons.navigate_next),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.nightlight),
+              title: const Text('다크 모드'),
+              trailing: Switch(
+                value: _isChanged,
+                onChanged: (value) {
+                  setState(() {});
+                  _isChanged = value;
+                },
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
       body: SafeArea(
         child: Center(
           child: Column(
